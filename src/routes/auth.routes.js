2089778c1a410dc164d4316a59d26fe64b6a899b
Akinur Rahman <akinurrahmanofficial@gmail.com>
2025-02-19 22:42:03 +0530

    remove verifyJWT middlware from refresh route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh").post(verifyJWT,refreshAccessToken)
+router.route("/refresh").post(refreshAccessToken)
 
 
 export default router;
